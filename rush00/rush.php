f7623e43305d7f5281191b0967f4e5cb256aabaa
Felecia MAKGOKA <fmakgoka@c4r9s1.wethinkcode.co.za>
2019-10-14 17:44:40 +0200

    rush

---
+++ rush00/rush.php
@@ -0,0 +1,31 @@
+<?php
+   //connect to db
+   $conn = mysqli_connect('localhost', 'root','Fele8686', 'mydatab');
+   //validation
+   if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+   $sql = "CREATE TABLE `mydatab`.`user table` ( 
+      `ID` INT NOT NULL AUTO_INCREMENT , `Username` VARCHAR(8) NOT NULL , 
+      `Email` VARCHAR(20) NOT NULL , `Password` VARCHAR(12) NOT NULL ,
+       PRIMARY KEY (`ID`))";
+      
+      if (mysqli_query($conn, $sql)) {
+          echo "Table Users created successfully";
+          echo "\n";
+      } else {
+          echo "Error creating table: " . mysqli_error($conn);
+      }
+   $sql = "CREATE TABLE `mydatab`.`store table` ( 
+      `ID` INT NOT NULL AUTO_INCREMENT , `Product Name` TEXT NOT NULL , 
+      `Price` INT NOT NULL , `Amount` INT NOT NULL , PRIMARY KEY (`ID`))";
+
+   if (mysqli_query($conn, $sql)) {
+      echo "Table Users created successfully\n";
+    } else {
+       echo "Error creating table: " . mysqli_error($conn);
+   }
+
+      mysqli_close($conn);
+   
+?>
